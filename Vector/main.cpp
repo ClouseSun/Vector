24d18ed23478179203efeb7c7172403bb8658142
ClouseSun <ClouseSun@gmail.com>
2016-03-07 18:58:17 +0800

    Initial Commit

---
+++ Vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vector
+//
+//  Created by 孙云霄 on 16/3/7.
+//  Copyright © 2016年 ClouseSun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
